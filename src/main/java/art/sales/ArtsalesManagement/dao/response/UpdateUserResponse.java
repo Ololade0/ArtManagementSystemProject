d1b9a322d3b9b0d2749970845bde6102c1533e10
Ololade0 <adesuyiololade@gmail.com>
2022-12-23 13:04:59 +0100

    Art Management system

--- src/main/java/art/sales/ArtsalesManagement/dao/response/UpdateUserResponse.java
+++ src/main/java/art/sales/ArtsalesManagement/dao/response/UpdateUserResponse.java
@@ -1,9 +1,6 @@
 package art.sales.ArtsalesManagement.dao.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
